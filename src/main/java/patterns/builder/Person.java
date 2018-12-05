2b5fcb64a6067f0f57338890fcb802b8ddcf797b
Safr <Aliaksei_Safronau@epam.com>
2018-12-05 17:13:54 +0300

    ''

---
+++ src/main/java/patterns/builder/Person.java
@@ -0,0 +1,42 @@
+package patterns.builder;
+
+import java.util.Optional;
+
+public class Person {
+    private String name;
+    //    private int age;
+    private Optional<Integer> age;
+
+    public Person(Builder builder) {
+        name = builder.name;
+        age = builder.age;
+    }
+
+    public static class Builder {
+        private String name;
+        //        private int age = 30;
+        private Optional<Integer> age = Optional.empty();
+
+
+        public Builder(String name) {
+            this.name = name;
+        }
+
+//        public Builder addAge(int age) {
+//            this.age = age;
+//            return this;
+//        }
+        public Builder addAge(int age) {
+            this.age = Optional.ofNullable(age);
+            return this;
+        }
+
+        public Person build() {
+            return new Person(this);
+        }
+    }
+
+    public static void main(String[] args) {
+        Person person = new Builder("Safronava").addAge(54).build();
+    }
+}
